e6e6ec1c51107b008079b89a0ce37d4b73b31a8c
Tudorin Danut <dan.tudorin@stud.acs.upb.ro>
2020-06-09 17:17:23 +0300

    Updates

---
+++ precache-manifest.8adcb2a25366fe5d3f9e9f99897c5ac2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5115cfe5ea70b334e7c280ba2ab45c35",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "681d1b4df34cb3de65a2",
+    "url": "/Todo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cec759c75d38d4e2fb21",
+    "url": "/Todo/static/css/main.6e9f95e5.chunk.css"
+  },
+  {
+    "revision": "681d1b4df34cb3de65a2",
+    "url": "/Todo/static/js/2.2fa47452.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/Todo/static/js/2.2fa47452.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cec759c75d38d4e2fb21",
+    "url": "/Todo/static/js/main.b75aca35.chunk.js"
+  },
+  {
+    "revision": "68276cd01d73a9f310bc",
+    "url": "/Todo/static/js/runtime-main.94671e95.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
